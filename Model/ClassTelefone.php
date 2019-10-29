21e0fab17fd916f73e4f30c273bc12daca99f3a6
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2019-10-29 16:55:44 -0300

    Salvando correções da tabela Telefone.

--- Model/ClassTelefone.php
+++ Model/ClassTelefone.php
@@ -5,7 +5,6 @@ class ClassTelefone {
     function getCod_telefone() {
         return $this->cod_telefone;
     }
-
     function getCod_contato() {
         return $this->cod_contato;
     }
@@ -16,13 +15,12 @@ class ClassTelefone {
         return $this->tipo;
     }
     
-
     function setCod_telefone($cod_telefone) {
         $this->cod_telefone = $cod_telefone;
     }
- 
-    function setCod_contato($contato) {
-        $this->contato = $contato;
+
+    function setCod_contato($cod_contato) {
+        $this->cod_contato = $cod_contato;
     }
     
     function setTelefone($telefone) {
